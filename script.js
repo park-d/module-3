3af49e7d8c10d36194ebe645b2324c9fa8de4a6e
park-d <54589582+park-d@users.noreply.github.com>
2022-08-31 15:43:27 -0500

    added all code, and comments for password generator functionality, want to clean it up to make code efficient and concise

--- script.js
+++ script.js
@@ -1,80 +1,70 @@
-// On button click, prompt user for password length
-  // if password length is valid (ie not blank, or words and the right length), move on to other criteria
-  // for other criteria, they can choose to include lower, upper, number, or special characters. must choose at least one
-  //generate random characters based on criteria
-  // display in the box given
+//////////////////////////////----------PSEUDO-CODE BEGINS HERE----------//////////////////////////////
 
+// button click -> prompt asking for pwd length -> check that input fits requirements -> throw error if not -> check which character users want to include -> add those to an array when they choose yes -> error check for picking at least one criteria -> throw alert if not at least one -> use new array from chosen criteria to pick a random [index] in the array -> add that character that matches random index to a string value each time for the length of the chosen pwd length
 
 //////////////////////////////----------MY CODE BEGINS HERE----------//////////////////////////////
 
-//defining the arrays that contain all possible characters
+//defining the arrays that contain all possible characters for generator
 var charactersLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var charactersUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var charactersNumeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var charactersSpecial = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', '<', '>', '+', '=', '-', '_', '[', ']', ':', ';', '?', '/'];
-var pwdCriteria = []
-var pwdLength = ""
 
-
-function pwdInputs() {
+function generatePassword() {
   //creating empty array in which to put the included arrays from above
-  pwdCriteria = [];
+  var pwdCriteria = [];
 
-  //initial prompt for password length
-  pwdLength = parseInt(prompt('Choose password length: Password must contain between 8 - 128 characters.'))
+  //initial prompt for password length and turning it into a number because anything in a prompt window is a string
+  var pwdLength = parseInt(prompt('Choose password length: Password must contain between 8 - 128 characters.'))
 
-  // error checking for password length criteria
-  if (pwdLength < 8 || pwdLength > 128) {
+  // error checking for password length criteria, if not above 8 or not below 128 or if empty
+  if ((pwdLength < 8) || (pwdLength > 128) || (!pwdLength)) {
     alert('Password must contain between 8 - 128 characters.');
-    return null;
-  }
-
-  if (!pwdLength) {  
-    alert('Please enter a valid number.');
     return null;
   }
   //end error checking
 
-  //prompting user to select which character types to include
+  //prompting user to select which character types to include and adding the characters to empty array if they choose OK
+  //if user chooses to include Uppercase add to array pwdCriteria
   var includesLowercase = confirm('Click OK to include lowercase characters.');
-  var includesUppercase = confirm('Click OK to include uppercase characters.');
-  var includesNumeric = confirm('Click OK to include numeric characters.');
-  var includesSpecial = confirm('Click OK to include special characters.');
-
-  // using if logic to put the entire array into an empty array if the criteria was chosen (for each criteria chosen)
-  if(includesLowercase == true){
+  if (includesLowercase == true) {
     pwdCriteria = pwdCriteria.concat(charactersLowercase);
   }
-
-  if(includesUppercase == true){
+  //if user chooses to include Lowercase add to array pwdCriteria
+  var includesUppercase = confirm('Click OK to include uppercase characters.');
+  if (includesUppercase == true) {
     pwdCriteria = pwdCriteria.concat(charactersUppercase);
   }
-
-  if(includesNumeric == true){
+  //if user chooses to include Numeric add to array pwdCriteria
+  var includesNumeric = confirm('Click OK to include numeric characters.');
+  if (includesNumeric == true) {
     pwdCriteria = pwdCriteria.concat(charactersNumeric);
   }
-
-  if(includesSpecial == true){
+  //if user chooses to include Special add to array pwdCriteria
+  var includesSpecial = confirm('Click OK to include special characters.');
+  if (includesSpecial == true) {
     pwdCriteria = pwdCriteria.concat(charactersSpecial);
   }
 
-  // error checking for including at least one character type
+  // error checking the above prompts to check that at least one character type is included
   if (!includesLowercase && !includesUppercase && !includesNumeric && !includesSpecial) {
     alert('You must choose at least one character type.');
     return null;
   }
-  // end error checking
+  //end error checking
 
-  return null;
-}
 
-function generatePassword(){
+  //create empty variable to store randomly generated password string choices
   var pwdGenerated = "";
 
+  //creating a new variable to store randomly generated number between 0 and pwdLength
+  //looping as many times as pwdLength to create a string that pulls a a random character from pwdCriteria and adds it to pwd Generated.
+  //finally returns pwdGenerated so we can show password to user
   for (var i = 0; i < pwdLength; i++) {
     var charactersRandom = Math.floor(Math.random() * pwdCriteria.length);
     pwdGenerated = pwdGenerated + pwdCriteria[charactersRandom];
   }
+
   return pwdGenerated;
 }
 
@@ -82,19 +72,15 @@ function generatePassword(){
 
 var generateBtn = document.querySelector('#generate');
 
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-
 // Write password to the #password input
 function writePassword() {
-  //added second fuction to call here
-  pwdInputs();
+
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
-
